243189ea2644af5bf2588787504f5bea4f23cd3e
Iago Duarte <iagodsv@gmail.com>
2020-08-05 23:54:51 -0300

    Updates

---
+++ precache-manifest.fd142bda8c07207ae12e26be326250e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f071fc9fab8d39c59ed8fe980520ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4324a1db3c291e3f942",
+    "url": "/static/css/main.7f624a7c.chunk.css"
+  },
+  {
+    "revision": "8575e92d4f03d1003d9d",
+    "url": "/static/js/2.5796380b.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.5796380b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4324a1db3c291e3f942",
+    "url": "/static/js/main.afbce559.chunk.js"
+  },
+  {
+    "revision": "4d9af2a9620265095b18",
+    "url": "/static/js/runtime-main.e8bda96d.js"
+  }
+]);
